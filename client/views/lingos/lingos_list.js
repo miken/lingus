2ae24abc314ab30d5b99878a39c3f11477f609b5
Mike Nguyen <mike@toch.im>
2014-05-11 22:43:23 -0400

    Added dummy data and improved UI

---
+++ client/views/lingos/lingos_list.js
@@ -0,0 +1,24 @@
+var lingosData = [
+  {
+    name: 'halle berry hallelujah'
+  },
+  {
+    name: 'in a while crocodile'
+  },
+  {
+    name: 'next level'
+  },
+  {
+    name: 'later alligator'
+  },
+  {
+    name: 'peace out a-town'
+  },
+  {
+    name: 'sorry charlie'
+  },
+];
+
+Template.lingosList.helpers({
+  lingos: lingosData
+});
